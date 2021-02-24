439f17362243f23a87e7da83eb319424135cdcf9
ashen007 <asheniranga@gmail.com>
2021-02-24 12:46:54 +0530

    bin split function added

--- model.py
+++ model.py
@@ -1,4 +1,15 @@
-class TreeNode():
+import numpy as np
+
+
+def bin_split_df(data, feature, value):
+    mat0 = data.iloc[np.nonzero(data[feature].values > value)[0],:].iloc[0]
+    mat1 = data.iloc[np.nonzero(data[feature].values <= value)[0], :].iloc[0]
+    return mat0, mat1
+
+def create_tree():
+
+
+class TreeNode:
     def __init__(self, feature, value, left, right):
         self.feature_to_split = feature
         self.value_of_split = value
