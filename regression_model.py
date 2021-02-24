855f1fe50707288cb6629c43397ba8063ac921cc
ashen007 <asheniranga@gmail.com>
2021-02-24 17:30:10 +0530

    construct RootNode class

--- regression_model.py
+++ regression_model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 class RootNode:
@@ -26,4 +27,6 @@ class RootNode:
             left, right, thresh = self.__bin_split(i)
             rmse = self.__calculate_RMSE(left, right)
             self.__tree.append([thresh, rmse])
-        return [self.__tree, ]
+        self.__tree = pd.DataFrame(self.__tree, columns=['thresh', 'cost'])
+        best_root = self.__tree[self.__tree['cost'] == np.min(self.__tree['cost'])].thresh.values[0]
+        return [self.__tree, best_root]
