3fa6ac8b2cdf766adf0d3ba40a558fd36d4223d7
ashen007 <asheniranga@gmail.com>
2021-03-09 12:36:24 +0530

    CART algorithm made from recursion, so it can't rewrite with iteration so control depth introduce max_depth hyper-parameter

--- regression_model.py
+++ regression_model.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 class RegressionTree:
-    def __init__(self, x, y, data, interval, max_depth):
+    def __init__(self, x, y, data, interval, max_depth=0):
         self.x = x
         self.y = y
         self.data = data
@@ -12,8 +12,8 @@ class RegressionTree:
 
 
 class BestSplit(RegressionTree):
-    def __init__(self, x, y, data, interval, max_depth):
-        super().__init__(x, y, data, interval, max_depth)
+    def __init__(self, x, y, data, interval,max_depth):
+        super().__init__(x, y, data, interval,max_depth)
         self.tree = list()
 
     def bin_split(self, i, feat):
@@ -42,109 +42,12 @@ class BestSplit(RegressionTree):
             best_root = best_root.sample(1)
         return [best_root.feature.values[0], best_root.thresh.values[0], self.tree]
 
-
-# class RootNode:
-#     def __init__(self, data, feature, target, start):
-#         self.tree = list()
-#         self.start_point = start
-#         self.feature = feature
-#         self.target = target
-#         self.dataset = data
-
-# def __bin_split(self, i):
-#     thresh = np.mean(self.dataset[self.feature].iloc[i:i + self.start_point])
-#     lSet = self.dataset[self.dataset[self.feature] < thresh]
-#     rSet = self.dataset[self.dataset[self.feature] >= thresh]
-#     return lSet, rSet, thresh
-
-# def __calculate_RMSE(self, l, r):
-#     l_avg = np.mean(l[self.target])
-#     r_avg = np.mean(r[self.target])
-#     RMSE = np.sum(np.sqrt((l[self.target] - l_avg) ** 2)) + np.sum(np.sqrt((r[self.target] - r_avg) ** 2))
-#     return RMSE
-
-# def best_split(self):
-#     for i in range(0, self.dataset.shape[0] - self.start_point):
-#         left, right, thresh = self.__bin_split(i)
-#         rmse = self.__calculate_RMSE(left, right)
-#         self.tree.append([thresh, rmse])
-#     self.tree = pd.DataFrame(self.tree, columns=['thresh', 'cost'])
-#     best_root = self.tree[self.tree['cost'] == np.min(self.tree['cost'])].thresh.values[0]
-#     return [self.tree, best_root]
-
-
-# class TreeBuilder:
-#     def __init__(self, data, feature, target, start, break_point):
-#         self.data = data
-#         self.feature = feature
-#         self.target = target
-#         self.start = start
-#         self.break_point = break_point
-#
-#     def bin_split(self, data):
-#         node = RootNode(data, self.feature, self.target, self.start).best_split()[1]
-#         left = data[data[self.feature] < node]
-#         right = data[data[self.feature] >= node]
-#
-#         return left, right, node
-#
-#     def builder(self, data):
-#         lSet, rSet, node = self.bin_split(data)
-#         Tree = {'node': node}
-#
-#         if lSet.shape[0] >= self.break_point:
-#             Tree['left'] = self.builder(lSet)
-#         else:
-#             Tree['left'] = np.mean(lSet[self.target])
-#
-#         if rSet.shape[0] >= self.break_point:
-#             Tree['right'] = self.builder(rSet)
-#         else:
-#             Tree['right'] = np.mean(rSet[self.target])
-#
-#         return Tree
-
-
-# class BestRootNode:
-#     def __init__(self, data, feature, target, start):
-#         self.tree = list()
-#         self.start_point = start
-#         self.feature = feature
-#         self.target = target
-#         self.dataset = data
-
-# def bin_split(self, i, feat):
-#     thresh = np.mean(self.dataset[feat].iloc[i:i + self.start_point])
-#     lSet = self.dataset[self.dataset[feat] < thresh]
-#     rSet = self.dataset[self.dataset[feat] >= thresh]
-#     return lSet, rSet, thresh
-
-# def calculate_RMSE(self, l, r):
-#     l_avg = np.mean(l[self.target])
-#     r_avg = np.mean(r[self.target])
-#     RMSE = np.sum(np.sqrt((l[self.target] - l_avg) ** 2)) + np.sum(np.sqrt((r[self.target] - r_avg) ** 2))
-#     return RMSE
-
-# def best_split(self):
-#     for feat in self.feature:
-#         for i in range(0, self.dataset.shape[0] - self.start_point):
-#             left, right, thresh = self.bin_split(i, feat)
-#             rmse = self.calculate_RMSE(left, right)
-#             self.tree.append([feat, thresh, rmse])
-#     self.tree = pd.DataFrame(self.tree, columns=['feature', 'thresh', 'cost'])
-#     best_root = self.tree[self.tree['cost'] == np.min(self.tree['cost'])]
-#
-#     if best_root.shape[0] > 1:
-#         best_root = best_root.sample(1)
-#     return best_root.feature.values[0], best_root.thresh.values[0]
-
-
 class TreeBuilder(BestSplit):
-    def __init__(self, x, y, data, interval, max_depth):
-        super().__init__(x, y, data, interval, max_depth)
+    def __init__(self, x, y, data, interval,max_depth):
+        super().__init__(x, y, data, interval,max_depth)
 
     def bin_split(self, data, **kwargs):
-        feature, node = BestSplit(data=data, x=self.x, y=self.y, interval=self.interval,max_depth=self.depth).best_split()
+        feature, node = BestSplit(data=data, x=self.x, y=self.y, interval=self.interval,max_depth=5).best_split()[:2]
         left = data[data[feature] < node]
         right = data[data[feature] >= node]
 
@@ -153,8 +56,10 @@ class TreeBuilder(BestSplit):
     def builder(self, data, depth=0):
         lSet, rSet, node, feature = self.bin_split(data, )
         Tree = {'node': node, 'feature': feature, 'left': np.mean(lSet[self.y])}
-        if depth <= self.depth:
-            Tree['right'] = self.builder(rSet)
+
+        if depth<self.depth:
+            Tree['right'] = self.builder(rSet,depth+1)
+        else:
             Tree['right'] = np.mean(rSet[self.y])
 
         return Tree
